de0dc51155ca5b16b059379e5929b226167457a5
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2023-06-01 20:44:19 -0300

    Add files via upload

--- insercao_dados.sql
+++ insercao_dados.sql
@@ -1,5 +1,5 @@
 insert into localizacao(local,rua,bairro) value
 ("Casa Professor Elton","Rua Maria Bertoldi","Água Verde"),
 ("Senai","Rua Walter Marquart","Rio Molha"),
-(null,"Rua Hoje Morreu","Rio a Luz"),
+(null,"Rua Hoje Morreu","Rio a Noite"),
 ("Senai","Walter Marquart","Rio da Luz");
