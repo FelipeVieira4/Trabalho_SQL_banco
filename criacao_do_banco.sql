30aa973fe6c4f7ca890b64535d926676d67bf825
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2023-06-01 20:09:46 -0300

    Add files via upload

---
+++ criacao_do_banco.sql
@@ -0,0 +1,50 @@
+create database crimes;
+use crimes;
+
+# Criação da tablea "localização"
+create table localizacao(
+	id int primary key auto_increment,
+    localC varchar(255) not null,
+    rua varchar(255) not null,
+    bairro varchar(255) not null
+);
+
+# Criação da tablea "crimes"
+create table crime(
+	id int primary key auto_increment,
+    data date not null,
+    tipoCrime enum('Assalto','Roubo','Jogar LOL','Matar Aula') not null,
+    id_local int not null,foreign key(id_local) references localizacao(id)
+);
+
+#Criação da tebela "vitima"
+create table vitima(
+	id int primary key auto_increment,
+    nome varchar(255) not null,
+    cpf varchar(255) not null,
+    id_crime int,foreign key(id_crime) references crime(id) 
+);
+
+#Criação da tebela "suspeito"
+create table suspeito(
+	id int primary key auto_increment,
+    nome varchar(255) not null,
+    CPF varchar(11) not null,
+    telefone varchar(11),
+    idade int not  null
+);
+
+#Criação da relação crime e suspeito
+create table crime_suspeito(
+	id int primary key auto_increment,
+    id_crime int not null,foreign key(id_crime) references crime(id),
+    id_suspeito int not null,foreign key(id_suspeito) references suspeito(id)
+);
+
+#Criação da tabela "evidencia"
+create table evidencia(
+	id int primary key auto_increment,
+    iten varchar(255) not null,
+    id_crime int not null,foreign key(id_crime) references crime(id)
+);
+
---
+++ derrubar_tabelas.sql
@@ -0,0 +1,6 @@
+drop table localizacao;
+drop table crime;
+drop table vitima;
+drop table suspeito;
+drop table crime_suspeito;
+drop table evidencia;
