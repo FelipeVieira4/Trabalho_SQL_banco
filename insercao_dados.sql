73f255b37ee17bab79c61e4d101506be9a782e60
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2023-06-02 17:23:34 -0300

    Criando algumas inserçoes

--- insercao_dados.sql
+++ insercao_dados.sql
@@ -1,22 +1,18 @@
 insert into localizacao(local,rua,bairro) value
-("Casa Professor Elton","Rua Maria Bertoldi","Água Verde"),
-("Senai","Rua Walter Marquart","Rio Molha"),
-(null,"Rua Hoje Morreu","Rio a Noite"),
-("Senai","Walter Marquart","Rio da Luz");
+("Casa do Elton","R. Maria Bertoldi","Água Verde"),
+("Senai","R. Walter Marquart","Rio Molha"),
+("Casa 483","R. João Klein","Vila Lenzi"),
+("Casa 847","R. Affonso Nicoluzzi","RAU"),
+(null,"Av. Getúlio Vargas","Centro"),
+(null,"R. Hoje Morreu","Rio a Noite");
 
 insert into suspeito (nome,CPF,telefone,idade) value
-("Elton","66666666624","47666666666",76);
-insert into suspeito (nome,CPF,telefone,idade) value
-("Talles","07869645692","47963577418",45);
-insert into suspeito (nome,CPF,telefone,idade) value
-("Leael","08439884555","47896654123",39);
-insert into suspeito (nome,CPF,telefone,idade) value
-("Felipe","09509604724","47852247963",24);
-insert into suspeito (nome,CPF,telefone,idade) value
-("Gabi","17994325896","47963258741",30);
-insert into suspeito (nome,CPF,telefone,idade) value
-("Joao","07406924926","47996543998",26);
-insert into suspeito (nome,CPF,telefone,idade) value
+("Elton","66666666624","47666666666",76),
+("Talles","07869645692","47963577418",45),
+("Leael","08439884555","47896654123",39),
+("Felipe","09509604724","47852247963",24),
+("Gabi","17994325896","47963258741",30),
+("Joao","07406924926","47996543998",26),
 ("Tomas","08960871378","47975586456",18);
 
 
