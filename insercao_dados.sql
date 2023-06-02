8c5bcaa39df97e409f249dc23d9df6779c8a51c2
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2023-06-01 21:27:37 -0300

    Add files via upload

--- insercao_dados.sql
+++ insercao_dados.sql
@@ -3,3 +3,22 @@ insert into localizacao(local,rua,bairro) value
 ("Senai","Rua Walter Marquart","Rio Molha"),
 (null,"Rua Hoje Morreu","Rio a Noite"),
 ("Senai","Walter Marquart","Rio da Luz");
+
+insert into suspeito (nome,CPF,telefone,idade) value
+("Elton","66666666624","47666666666",76);
+insert into suspeito (nome,CPF,telefone,idade) value
+("Talles","07869645692","47963577418",45);
+insert into suspeito (nome,CPF,telefone,idade) value
+("Leael","08439884555","47896654123",39);
+insert into suspeito (nome,CPF,telefone,idade) value
+("Felipe","09509604724","47852247963",24);
+insert into suspeito (nome,CPF,telefone,idade) value
+("Gabi","17994325896","47963258741",30);
+insert into suspeito (nome,CPF,telefone,idade) value
+("Joao","07406924926","47996543998",26);
+insert into suspeito (nome,CPF,telefone,idade) value
+("Tomas","08960871378","47975586456",18);
+
+
+
+
