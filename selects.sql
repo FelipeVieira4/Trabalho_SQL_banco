3825e485ee7b04dd2102831bfccfce5b65fd1e1b
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2023-06-04 18:02:41 -0300

    Update selects.sql

--- selects.sql
+++ selects.sql
@@ -1,8 +1,3 @@
-select crime.tipoCrime, suspeito.nome, vitima.nome from crime_suspeito
-inner join crime on crime_suspeito.id_crime=crime.id
-inner join suspeito on crime_suspeito.id_suspeito = suspeito.id
-inner join vitima on vitima.id_crime=crime.id;
-
 select crime.tipoCrime as "Crime", suspeito.nome as "Suspeito", vitima.nome as "Vitima" from crime_suspeito
 inner join crime on crime.id = crime_suspeito.id_crime
 inner join suspeito on suspeito.id = crime_suspeito.id_suspeito
