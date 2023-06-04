5814e30c338ad0ecc93b03815ef5fbec30cba73f
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2023-06-04 18:01:20 -0300

    Update selects.sql

--- selects.sql
+++ selects.sql
@@ -7,3 +7,7 @@ select crime.tipoCrime as "Crime", suspeito.nome as "Suspeito", vitima.nome as "
 inner join crime on crime.id = crime_suspeito.id_crime
 inner join suspeito on suspeito.id = crime_suspeito.id_suspeito
 inner join vitima on vitima.id_crime = crime.id; #where suspeito.idade < 30;
+
+select crime.tipoCrime as "Crime", suspeito.nome as "Suspeito" from crime_suspeito
+inner join crime on crime.id = crime_suspeito.id_crime
+inner join suspeito on suspeito.id = crime_suspeito.id_suspeito;
